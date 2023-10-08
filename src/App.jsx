ae8fa9de3d9dca7453e1c144d271c51d4a947e10
Alireza Kafshdartoosi <71192730+AlirezaKToosi@users.noreply.github.com>
2023-10-08 11:45:47 +0200

    Add product details page to unlogged router and implement product detail pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,14 @@ import { Outlet } from "react-router-dom";
 import { NavigationBar, Footer } from "./components";
 import ItemsEndingSoon from "./components/ItemsEndingSoon";
 import LatestItems from "./components/LatestItems";
-import { ShowItem } from "./components/ShowItem";
 
 export default function App() {
-  const customMatch = {
-    params: {
-      id: "1", // You can set it to any desired value
-    },
-  };
   return (
     <section className="App">
       <NavigationBar />
       <Outlet />
       <LatestItems />
       <ItemsEndingSoon />
-      <ShowItem match={customMatch} />
       <Footer />
     </section>
   );
--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,9 +1,76 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom"; // Add this import
+import PlaceHolderImage from "../assets/placeholder.jpg";
+
 export function ProductDetailsPage() {
-  return (
-    <article className="page product-details-page">
-      <div className="container">
-        <h2>Product details page</h2>
+  const { productId } = useParams(); // Get the productId from the URL params
+  const [item, setItem] = useState(null);
+  const [timeRemaining, setTimeRemaining] = useState(null);
+
+  // Function to fetch item details
+  const fetchItemDetails = async () => {
+    try {
+      const url = `${import.meta.env.VITE_API_URL}/item/${productId}`; // Use the productId from URL
+
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Failed to fetch item details");
+      }
+      const data = await response.json();
+      setItem(data); // Set the item data in state
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    // Fetch item details from the API based on the productId when the component mounts
+    fetchItemDetails();
+  }, [productId]); // Add productId to the dependency array
+
+  useEffect(() => {
+    // Calculate time remaining when item data is available
+    if (item) {
+      const endDateTime = new Date(item.endDateTime);
+      const currentTime = new Date();
+      const timeDifference = endDateTime - currentTime;
+
+      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor(
+        (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+      );
+
+      setTimeRemaining({ days, hours, minutes });
+    }
+  }, [item]);
+
+  // Function to render item details
+  const renderItemDetails = () => {
+    if (!item || !timeRemaining) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div className="product-detail">
+        <div className="product-image">
+          <img src={item.image_url || PlaceHolderImage} alt="Product Image" />
+        </div>
+        <div className="product-info">
+          <h1>{item.title}</h1>
+          <p>Description: {item.description}</p>
+          <p>Leading Bid (SEK): {item.startPrice}</p>
+          <p>
+            Ends in: {timeRemaining.days} days, {timeRemaining.hours} hours,{" "}
+            {timeRemaining.minutes} minutes
+          </p>
+          <button id="b">Place Bid</button>
+        </div>
       </div>
-    </article>
-  );
+    );
+  };
+
+  return renderItemDetails();
 }
